537c81a0b93caf3c6892a8c69db9cd47c6249cb2
ayshdigitalstudios <adstudios.trp@gmail.com>
2023-03-04 18:50:01 +0530

    new changes

--- src/Pages/Components/Navbar.jsx
+++ src/Pages/Components/Navbar.jsx
@@ -1,21 +1,64 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { NavmenuItems } from "./NavmenuItems";
 
 const Navbar = () => {
-
+ const location = useLocation()
 
   return (
     <header className="header">
       <div className="row m-0">
-        <div className="col-lg-4">Logo</div>
-        <div className="col-lg-4">
-          {NavmenuItems.map((item, index) => (
-            <Link key={index} to={item.title.toLowerCase()}>
-              {item.title}
-            </Link>
-          ))}
-        </div>
-        <div className="col-lg-4">Icon</div>
+        <nav className="navbar bg-body-tertiary  fixed-top">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">
+              Offcanvas navbar
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="offcanvas"
+              data-bs-target="#offcanvasNavbar"
+              aria-controls="offcanvasNavbar"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div
+              className="offcanvas offcanvas-end w-75"
+              tabIndex="-1"
+              id="offcanvasNavbar"
+              aria-labelledby="offcanvasNavbarLabel"
+            >
+              <div className="offcanvas-header">
+                <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
+                  Offcanvas
+                </h5>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="offcanvas"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="offcanvas-body">
+                <ul className="navbar-nav justify-content-end flex-grow-1 pe-3 gap-1">
+                  {NavmenuItems.map((item, index) => (
+                    <li className="nav-item" key={index}>
+                      <Link
+                        to={item.title.toLowerCase()}
+                        className={
+                          location.pathname.split("/")[1] === item.title.toLowerCase()
+                            ? "nav-link active"
+                            : "nav-link"
+                        }
+                      >
+                        {item.title}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </nav>
       </div>
     </header>
   );
